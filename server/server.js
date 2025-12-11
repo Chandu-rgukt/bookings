611473b2066a5ceb7b0c882a25f9108bd5068ebb
chandu <chandrasekharjnvnellore@gmail.com>
2025-12-11 19:55:23 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const { connect } = require('./config/db');
 const routes = require('./routes/index');
 const app = express();
 app.use(cors());
+
+// Set permissive CSP headers for API responses (override any restrictive defaults)
+app.use((req, res, next) => {
+  // Only set CSP for HTML responses, not API JSON responses
+  if (req.path.startsWith('/api')) {
+    // Don't set CSP for API routes
+    return next();
+  }
+  // For static files or HTML, allow inline styles
+  res.setHeader('Content-Security-Policy', "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: http: data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http:; style-src 'self' 'unsafe-inline' https: http:; img-src 'self' data: https: http:; font-src 'self' data: https: http:; connect-src 'self' https: http: ws: wss:;");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use('/api', routes);
 const port = process.env.PORT || 4000;
